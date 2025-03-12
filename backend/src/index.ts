249633541f8ba07bb8a143e84948303b9bb256f8
Bharath <nallagondlabharath159@gmail.com>
2025-03-12 06:46:41 +0530

    Removed / route in the backend index.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,11 +12,8 @@ app.use(express.json());
 app.use("/api/auth",authRouter);
 
 
-app.get("/",(req: Request,res: Response)=>{
-    res.send("Hello World");
-})
 
 app.listen(port,()=>{
-    connectDb();
+    connectDb();``
     console.log(`Server is running on http://localhost:${port}`);
 })
