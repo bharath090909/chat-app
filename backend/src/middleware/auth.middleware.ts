fbae14ec31e7c864ca81b0b8c0cf2148d77d6e5e
Bharath <nallagondlabharath159@gmail.com>
2025-03-28 06:54:15 +0530

    Commit

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -8,17 +8,22 @@ export const authMiddleware = async (
 ) => {
   const token: string = req.cookies.accessToken;
   console.log(token, "hello90428908");
-  if (!token) {
-    res.status(401).json({ message: "Token is Missing" });
-    return;
-  }
-  const secret: string = process.env.JWT_SECRET || "123";
-  const decodedToken = jwt.verify(token, secret) as JwtPayload;
+  try {
+    if (!token) {
+      res.status(401).json({ message: "Token is Missing" });
+      return;
+    }
+    const secret: string = process.env.JWT_SECRET || "123";
+    const decodedToken = jwt.verify(token, secret) as JwtPayload;
 
-  if (typeof decodedToken != "string" && decodedToken?.userId) {
-    req.userId = decodedToken?.userId;
-    next();
-  } else {
-    res.status(401).json({ message: "Unauthorized" });
+    if (decodedToken?.userId) {
+      (req as any).userId = decodedToken?.userId;
+      next();
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
